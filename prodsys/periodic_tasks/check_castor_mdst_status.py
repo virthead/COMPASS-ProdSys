289f465b1942638f9de9370e7a69c42271361f37
root <root@vm221-123.jinr.ru>
2018-09-29 11:52:33 +0300

    24 hours to 4 hours per retry

--- prodsys/periodic_tasks/check_castor_mdst_status.py
+++ prodsys/periodic_tasks/check_castor_mdst_status.py
@@ -86,9 +86,9 @@ def check_files_on_castor():
                     
                     if result.find(test) == -1:
                         diff = datetime.datetime.now().replace(tzinfo=None) - c[3].replace(tzinfo=None)
-                        logger_task.info('File %s was not delivered, transfer was submitted at %s which is %s days from now' % (test, c[3], diff.days))
-                        if diff.days >= 1:
-                            logger.info('Transfer request was performed in more than 24 hours ago, going to restart it')
+                        logger_task.info('File %s was not delivered, transfer was submitted at %s which is %s hours from now' % (test, c[3], (diff.seconds/3600)))
+                        if diff.seconds/3600 >= 4:
+                            logger.info('Transfer request was performed in more than 4 hours ago, going to restart it')
                             restart_transfer(logger_task, t[0], c[1], c[2])
                         
                             continue
